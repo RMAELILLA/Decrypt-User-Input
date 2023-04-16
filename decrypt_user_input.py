7567c8001bc00ae53c70c2521bd4829f17ba30b5
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-16 15:09:21 +0800

    let user input

--- decrypt_user_input.py
+++ decrypt_user_input.py
@@ -1,6 +1,7 @@
 # Assignment 2 Problem 2
 
 # Let user input encrypted text
+encrypt_input = ("Enter sentence you need to decrypt: ")
 # Evaluate encrypted text
 #   if input text encrypted by * change a
 #   if input text encrypted by & change e
