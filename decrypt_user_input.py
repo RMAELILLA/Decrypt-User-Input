7c1f1750c7f875779a001cdabc1ad259dd1c65eb
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-16 15:06:38 +0800

    decrypt input using pseudocode

---
+++ decrypt_user_input.py
@@ -0,0 +1,10 @@
+# Assignment 2 Problem 2
+
+# Let user input encrypted text
+# Evaluate encrypted text
+#   if input text encrypted by * change a
+#   if input text encrypted by & change e
+#   if input text encrypted by # change i
+#   if input text encrypted by + change o
+#   if input text encrypted by ! change u
+# display decrypted
