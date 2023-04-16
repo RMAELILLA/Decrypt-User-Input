7fa643f6288b79e9dc06df72a42cc68a4f208cb0
RMAELILLA <ronioryzamae12@gmail.com>
2023-04-16 15:10:28 +0800

    check input length

--- decrypt_user_input.py
+++ decrypt_user_input.py
@@ -3,6 +3,7 @@
 # Let user input encrypted text
 encrypt_input = ("Enter sentence you need to decrypt: ")
 # Evaluate encrypted text
+for i in range(len(encrypt_input)):
 #   if input text encrypted by * change a
 #   if input text encrypted by & change e
 #   if input text encrypted by # change i
